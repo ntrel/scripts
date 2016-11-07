f77a4867a53fdf8d8a1eb2a124a1e8e50b633ace
Nick Treleaven <nick.treleaven@btinternet.com>
2016-11-07 11:30:17 +0000

    git-checkout: Make no args pull master

--- git-checkout.sh
+++ git-checkout.sh
@@ -1,5 +1,6 @@
 if [[ $1 == '-h' ]]; then
     echo "Fetch, checkout and branch (optional)"
+    echo "$0    # checkout, pull master"
     echo "$0 [-b] [BRANCH]"
     echo "$0 [-b] REPO BRANCH"
     exit
@@ -9,11 +10,17 @@ if [[ $1 == '-b' ]]; then CO=1; shift; fi
 REPO=origin
 BRANCH=master
 
+if [[ $1 == "" ]]; then
+    [[ $CO == "" ]] || exit;
+    git checkout master || exit;
+    git pull --ff-only;
+    exit;
+fi
 if [[ $2 != "" ]]; then
     REPO=$1
     BRANCH=$2
 else
-    [[ $1 != "" ]] && BRANCH=$1
+    BRANCH=$1
 fi
 
 git fetch $REPO $BRANCH
