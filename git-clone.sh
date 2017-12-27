ce225e4131627873999ab12d79f5c56bfda5df2b
Nick Treleaven <n@trelsoft.com>
2017-12-27 11:43:14 +0000

    Add remote for git-clone

--- git-clone.sh
+++ git-clone.sh
@@ -1,6 +1,12 @@
-CMD=https://github.com/$1/$2
-[[ $1 == '-g' ]] && shift && CMD="git@github.com:$1/$2.git"
+URL=https://github.com/$1/$2
+[[ $1 == '-g' ]] && shift && URL="git@github.com:$1/$2.git"
 
 [[ $2 == '' ]] && echo "Usage: $0 [-g] user reponame" && exit
 echo Cloning $1/$2:
-git clone $CMD
+git clone $URL
+
+case $URL in h*)
+	URL="git@github.com:ntrel/$2.git"
+	echo Adding remote:
+	git remote add ntrel $URL
+esac
