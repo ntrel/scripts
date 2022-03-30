59fe2ac5eec4096a5556fa1df11d02b93117e54f
Nick Treleaven <ntrel002@gmail.com>
2022-03-30 13:34:52 +0100

    clone: -1 option

--- git-clone.sh
+++ git-clone.sh
@@ -1,9 +1,11 @@
 URL=https://github.com/$1/$2
 [[ $1 == '-g' ]] && shift && URL="git@github.com:$1/$2.git"
+# note this makes fetch slower
+[[ $1 == '-1' ]] && shift && OPT='--depth 1'
 
 [[ $2 == '' ]] && echo "Usage: $0 [-g] user reponame" && exit
 echo Cloning $1/$2:
-git clone $URL
+git clone $URL $OPT
 cd $2
 
 case $URL in h*)
