9e98da63a114b8a82bd032de7679e9f2c0343696
Nick Treleaven <ntrel002@gmail.com>
2022-09-14 12:32:15 +0100

    Fix broken options

--- git-clone.sh
+++ git-clone.sh
@@ -1,15 +1,17 @@
-URL=https://github.com/$1/$2
-[[ $1 == '-g' ]] && shift && URL="git@github.com:$1/$2.git"
+GIT=git
+STEM=https://github.com/
+[[ $1 == '-g' ]] && shift && STEM="git@github.com:"
 # note this makes fetch slower
 [[ $1 == '-1' ]] && shift && OPT='--depth 1'
 
+URL=$STEM$1/$2
 [[ $2 == '' ]] && echo "Usage: $0 [-g] user reponame" && exit
-echo Cloning $1/$2:
-git clone $URL $OPT
+
+set -x #echo on
+$GIT clone $URL $OPT || exit
 cd $2
 
 case $URL in h*)
 	URL="git@github.com:ntrel/$2.git"
-	echo Adding remote:
-	git remote add ntrel $URL
+	$GIT remote add ntrel $URL
 esac
