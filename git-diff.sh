a4e0eb70998bfeab102deb1c5f36f07b29bfd59b
Nick Treleaven <ntrel002@gmail.com>
2023-04-23 12:53:21 +0100

    diff: Use head not cat

--- git-diff.sh
+++ git-diff.sh
@@ -1,10 +1,10 @@
+#!/usr/bin/env bash
 CMD='diff'
-[[ $1 == show ]] && CMD='show' && shift
 [[ $1 == -s ]] && CMD+=' --staged' && shift
 
 F=$(dirname $0)/git.diff
 git $CMD $@ >$F
-if [[ `cat $F` == '' ]]; then 
+if [[ `head $F` == '' ]]; then 
 	echo "No changes."
 else
 	geany $F
