19fd79f38b75d1f3c8aa99ded0014dc49a6e75ef
Nick Treleaven <ntrel002@gmail.com>
2025-11-24 10:26:01 +0000

    setup git global config

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# options
+git config --global commit.verbose 1
+
+# aliases
+git config --global alias.bd 'branch --sort=-committerdate'
+git config --global alias.g 'grep -F'
+git config --global alias.ph 'push ntrel HEAD'
+git config --global alias.rb 'rebase'
+git config --global alias.rba 'rebase --autostash'
+git config --global alias.s 'status -uno'
